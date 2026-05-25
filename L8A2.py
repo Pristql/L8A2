f5a1b9e58f1ea737a93984ebfc1f2f2d5e96824e
Pristql <luigimansion111@gmail.com>
2026-05-25 20:43:53 +0530

    Add files via upload

---
+++ L8A2.py
@@ -0,0 +1,7 @@
+
+a=int(input("Enter a number (numerator)"))
+b=int(input("Enter a number (denominator)"))
+if a%b==0:
+    print(a, "is divisble by", b)
+else:
+    print(a, "is not divisble by", b)
